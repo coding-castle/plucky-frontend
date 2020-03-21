96b9d83ed143feb6b44265263148b686cf3e12dc
Leon Zimmermann <leon.zimmermann@hotmail.com>
2020-03-21 13:18:07 +0100

    Add pages and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { EmployeeFeedComponent } from "./pages/employee-feed/employee-feed.component";
+import { RegisterFarmerComponent } from "./pages/register-farmer/register-farmer.component";
+import { FarmerFeedComponent } from "./pages/farmer-feed/farmer-feed.component";
+import { RegisterEmployeeComponent } from "./pages/register-employee/register-employee.component";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "register-farmer",
+    component: RegisterFarmerComponent
+  },
+  {
+    path: "register-employee",
+    component: RegisterEmployeeComponent
+  },
+  {
+    path: "farmer-feed",
+    component: FarmerFeedComponent
+  },
+  {
+    path: "employee-feed",
+    component: EmployeeFeedComponent
   }
 ];
 
--- src/app/pages/register-farmer/register-farmer.component.ts
+++ src/app/pages/register-farmer/register-farmer.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-register-farmer',
-  templateUrl: './register-farmer.component.html',
-  styleUrls: ['./register-farmer.component.scss']
+  selector: "app-register-farmer",
+  templateUrl: "./register-farmer.component.html",
+  styleUrls: ["./register-farmer.component.scss"]
 })
 export class RegisterFarmerComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onFarmerFeedClicked() {
+    this.router.navigate(["/farmer-feed"]);
   }
-
 }
