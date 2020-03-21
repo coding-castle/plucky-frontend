bc5749f9ceb222a5adf5fe63748bb38d451f5410
Leon Zimmermann <leon.zimmermann@hotmail.com>
2020-03-21 12:41:03 +0100

    generate registration and feed pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,13 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
 import { HomeComponent } from "./pages/home/home.component";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RegisterFarmerComponent } from './pages/register-farmer/register-farmer.component';
+import { RegisterEmployeeComponent } from './pages/register-employee/register-employee.component';
+import { EmployeeFeedComponent } from './pages/employee-feed/employee-feed.component';
+import { FarmerFeedComponent } from './pages/farmer-feed/farmer-feed.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, RegisterFarmerComponent, RegisterEmployeeComponent, EmployeeFeedComponent, FarmerFeedComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
