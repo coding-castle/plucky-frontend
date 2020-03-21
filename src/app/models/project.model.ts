6794104a420df18ca6477c6422d08f21afcabc85
jameswel <jameswelsh33@gmail.com>
2020-03-21 18:25:21 +0100

    Project Model

---
+++ src/app/models/project.model.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  description: string;
+  displayName: string;
+  employees: [];
+  endDate: Date;
+  farmerId: string;
+  maxEmployees: number;
+  startDate: Date;
+  uid: string;
+}
