ca64df908f1a9aad8e081166353e06cf7e69a77c
Leon Zimmermann <leon.zimmermann@hotmail.com>
2020-03-21 15:03:39 +0100

    Implement BottomNavBar Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,31 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
 import { HomeComponent } from "./pages/home/home.component";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { RegisterFarmerComponent } from './pages/register-farmer/register-farmer.component';
-import { RegisterEmployeeComponent } from './pages/register-employee/register-employee.component';
-import { EmployeeFeedComponent } from './pages/employee-feed/employee-feed.component';
-import { FarmerFeedComponent } from './pages/farmer-feed/farmer-feed.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { RegisterFarmerComponent } from "./pages/register-farmer/register-farmer.component";
+import { RegisterEmployeeComponent } from "./pages/register-employee/register-employee.component";
+import { EmployeeFeedComponent } from "./pages/employee-feed/employee-feed.component";
+import { FarmerFeedComponent } from "./pages/farmer-feed/farmer-feed.component";
+import { BottomNavComponent } from "./components/bottom-nav/bottom-nav.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, RegisterFarmerComponent, RegisterEmployeeComponent, EmployeeFeedComponent, FarmerFeedComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegisterFarmerComponent,
+    RegisterEmployeeComponent,
+    EmployeeFeedComponent,
+    FarmerFeedComponent,
+    BottomNavComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
