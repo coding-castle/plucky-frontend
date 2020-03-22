dd85d05fe15d965ba5102f1e8512a28bc315c55a
Patrick Goeler <patrickvongoeler@gmail.com>
2020-03-22 14:02:12 +0100

    add inputs to maps

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit, Output, EventEmitter, Input, NgModule } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  NgModule
+} from "@angular/core";
+import { Farm } from "src/app/models/farm.model";
 
 @Component({
   selector: "app-map",
   templateUrl: "./map.component.html",
-  styleUrls: ["./map.component.scss"],
+  styleUrls: ["./map.component.scss"]
 })
-
 export class MapComponent implements OnInit {
   zoom: Number = 18;
   @Input() lat: Number = 48.7791242;
   @Input() lng: Number = 9.0371322;
   @Input() editable = false;
+  @Input() farm: Farm;
 
   ngOnInit(): void {}
 }
