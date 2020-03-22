6e1f9f23fae10c25a63fbfb4c733bb0e63e81b28
Patrick Goeler <patrickvongoeler@gmail.com>
2020-03-22 19:31:33 +0100

    Remove Env Files and add ignore

---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,26 @@
+/**
+ * You need to create "environment.prod.ts" and "environment.ts" yourself
+ * and fill in the necessary data
+ */
+
+export const environmentSample = {
+  production: true,
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  googleMaps: {
+    apiKey: "",
+    libraries: [""]
+  },
+  mtp: {
+    host: "",
+    email: "",
+    pass: ""
+  }
+};
